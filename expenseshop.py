401f420fbbb62d5446f253271db5a60b9bec12ad
Joseph, Robin Philip <robin.rpj@gmail.com>
2017-02-26 05:02:59 +0100

    server beta

--- expenseshop.py
+++ expenseshop.py
@@ -6,18 +6,18 @@ import datetime, json
 
 app = Flask(__name__)
 
+'''
 app.config['MYSQL_HOST'] = 'rojo16.mysql.pythonanywhere-services.com'
 app.config['MYSQL_USER'] = 'rojo16'
 app.config['MYSQL_PASSWORD'] = 'hello123'
 app.config['MYSQL_DB'] = 'rojo16$testdb'
-
 '''
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'testuser'
 app.config['MYSQL_PASSWORD'] = 'test123'
 app.config['MYSQL_DB'] = 'testdb'
 
-'''
 
 mysql = MySQL(app)
 
@@ -31,33 +31,33 @@ def index():
 ##############################SHOPLIST PART############################
 
 
-@app.route('/shoplist/items')
-def get_items():
+@app.route('/<string:user>/shoplist/items')
+def get_items(user):
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT name, cost, priority from shoplist_table order by priority desc''')
+    cur.execute('''SELECT name, priority from %s_shoplist_table order by priority desc''' % user)
     retVal = cur.fetchall()
     sting = [i for i in retVal]
     # sting = [i for i in sting]
     spring = []
     for i in sting:
-        thing =dict(zip(["name", "cost", "priority"], i))
+        thing =dict(zip(["name", "priority"], i))
         spring.append(thing)
     return (json.dumps(spring))
 
 
-@app.route('/shoplist/items/<string:item>', methods=['POST', 'DELETE'])
-def manage_items(item):
+@app.route('/<string:user>/shoplist/items/<string:item>', methods=['POST', 'DELETE'])
+def manage_items(user, item):
     cur = mysql.connection.cursor()
 
     if request.method == 'POST':
-        cur.execute('''SELECT MAX(id) from shoplist_table''')
+        cur.execute('''SELECT MAX(id) from %s_shoplist_table''' %user)
         maxid = cur.fetchone()
         ID = maxid[0]
         if not maxid[0]:
             ID = -1
         items = item.split(',')
         for i, j in enumerate(items):
-            cur.execute('''INSERT INTO shoplist_table(id, name) value (%s ,"%s")''' %(ID+1+i, j))
+            cur.execute('''INSERT INTO %s_shoplist_table(id, name) value (%s ,"%s")''' %(user, ID+1+i, j))
             mysql.connection.commit()
         return "Added %s!" %item
 
@@ -65,39 +65,39 @@ def manage_items(item):
     if request.method == 'DELETE':
         items = item.split(',')
         for i, item in enumerate(items):
-            cur.execute('''DELETE from shoplist_table where name = "%s"''' %item)
+            cur.execute('''DELETE from %s_shoplist_table where name = "%s"''' %(user, item))
             mysql.connection.commit()
         return "Deleted item(s)!"
 
 
-@app.route('/shoplist/prioritize/<string:item>', methods=['PUT'])
-def prioritize_items(item):
+@app.route('/<string:user>/shoplist/prioritize/<string:item>', methods=['PUT'])
+def prioritize_items(user, item):
     cur = mysql.connection.cursor()
 
     if request.method == 'PUT':
 
         items = item.split(',')
         for i, item in enumerate(items):
-            cur.execute('''UPDATE shoplist_table set priority = "YES" where name = "%s"''' %item)
+            cur.execute('''UPDATE %s_shoplist_table set priority = "YES" where name = "%s"''' %(user, item))
             mysql.connection.commit()
         return "Set Priorities!"
 
 
-@app.route('/shoplist/unprioritize/<string:item>', methods=['PUT'])
-def unprioritize_items(item):
+@app.route('/<string:user>/shoplist/unprioritize/<string:item>', methods=['PUT'])
+def unprioritize_items(user, item):
     cur = mysql.connection.cursor()
 
     if request.method == 'PUT':
 
         items = item.split(',')
         for i, item in enumerate(items):
-            cur.execute('''UPDATE shoplist_table set priority = "NO" where name = "%s"''' %item)
+            cur.execute('''UPDATE %s_shoplist_table set priority = "NO" where name = "%s"''' %(user, item))
             mysql.connection.commit()
         return "Unset Priorities!"
 
-"""
-@app.route('/shoplist/price/<string:item>', methods=['PUT'])
-def price_items(item):
+
+@app.route('/<string:user>/shoplist/price/<string:item>', methods=['PUT'])
+def price_items(user, item):
     cur = mysql.connection.cursor()
 
     if request.method == 'PUT':
@@ -105,31 +105,60 @@ def price_items(item):
         items = item.split(',')
         for i, item in enumerate(items):
             tag = item.split(':')
-            cur.execute('''UPDATE shoplist_table set cost = "%s" where name = "%s"''' % (tag[0], tag[1]))
+            cur.execute('''UPDATE %s_shoplist_table set cost = "%s" where name = "%s"''' % (user, tag[0], tag[1]))
             mysql.connection.commit()
         return "Set Priorities!"
 
-"""
+
+
 
 #############################EXPENSE PART#####################################
 
-@app.route('/expense/items')
-def get_all_expenses1():
+@app.route('/<string:user>/expense/items1')
+def get_all_expenses(user):
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT name, cost, month, category from expense_table''')
+    cur.execute('''SELECT name, cost, month, category from %s_expense_table''' % user)
     retVal = cur.fetchall()
     sting1 = ""
 
     now = datetime.datetime.now()
     month = "%d%d" % (now.year, now.month)
     month = month[2:]
-    cur.execute('''SELECT mon_lim from limit_table where name = "DEFAULT"''')
+    cur.execute('''SELECT mon_lim from %s_limit_table where name = "DEFAULT"''' %user)
     lim_val = cur.fetchone()
-    cur.execute('''SELECT sum(cost) from expense_table where month = %s''' % month)
+    cur.execute('''SELECT sum(cost) from %s_expense_table where month = %s''' % (user, month))
     mon_val = cur.fetchone()
     lim = ""
     try:
-        #### GET THE LIMIT DIFFERENCE FROM EXPENSE
+        lim = str(lim_val[0] - mon_val[0])
+    except:
+        pass
+    sting = [i for i in retVal]
+    # sting = [i for i in sting]
+    spring = []
+    for i in sting:
+        thing =dict(zip(["name", "cost", "date", "category"], i))
+        thing['limit'] = lim
+        spring.append(thing)
+    return (json.dumps(spring))
+
+
+@app.route('/<string:user>/expense/items')
+def get_all_expenses1(user):
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT name, cost, month, category from %s_expense_table''' % user)
+    retVal = cur.fetchall()
+    sting1 = ""
+
+    now = datetime.datetime.now()
+    month = "%d%d" % (now.year, now.month)
+    month = month[2:]
+    cur.execute('''SELECT mon_lim from %s_limit_table where name = "DEFAULT"''' % user)
+    lim_val = cur.fetchone()
+    cur.execute('''SELECT sum(cost) from %s_expense_table where month = %s''' % (user, month))
+    mon_val = cur.fetchone()
+    lim = ""
+    try:
         lim = str(lim_val[0] - mon_val[0])
     except:
         lim = str(lim_val[0])
@@ -147,47 +176,47 @@ def get_all_expenses1():
 
 
 
-@app.route('/expense/total/<int:month>')
-def get_month_total(month):
+@app.route('/<string:user>/expense/total/<int:month>')
+def get_month_total(user, month):
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT sum(cost) from expense_table where month = %d''' % month)
+    cur.execute('''SELECT sum(cost) from %s_expense_table where month = %d''' % (user, month))
     retVal = cur.fetchone()
     return str(retVal[0])
 
 
 
-@app.route('/expense/items/<int:month>/<string:category>')
-def get_category_month_expenses(month, category):
+@app.route('/<string:user>/expense/items/<int:month>/<string:category>')
+def get_category_month_expenses(user, month, category):
     cur = mysql.connection.cursor()
     if month == 0:
-        cur.execute('''SELECT name, cost, month from expense_table where category = "%s"''' % category)
+        cur.execute('''SELECT name, cost, month from %s_expense_table where category = "%s"''' % (user, category))
     elif category == '0':
-        cur.execute('''SELECT name, cost, category from expense_table where month = %d''' % month)
+        cur.execute('''SELECT name, cost, category from %s_expense_table where month = %d''' % (user, month))
     else:
-        cur.execute('''SELECT name, cost from expense_table where month = %s and category = "%s"''' % (month, category))
+        cur.execute('''SELECT name, cost from %s_expense_table where month = %s and category = "%s"''' % (user, month, category))
     retVal = cur.fetchall()
     return str(retVal)
 
 
-"""
-@app.route('/expense/limit_bal')
-def get_month_limit():
+
+@app.route('/<string:user>/expense/limit_bal')
+def get_month_limit(user):
     cur = mysql.connection.cursor()
     now = datetime.datetime.now()
     month = "%d%d" % (now.year, now.month)
     month = month[2:]
-    cur.execute('''SELECT mon_lim from limit_table where name = "DEFAULT"''')
+    cur.execute('''SELECT mon_lim from %s_limit_table where name = "DEFAULT"''' % user)
     lim_val = cur.fetchone()
-    cur.execute('''SELECT sum(cost) from expense_table where month = %s''' % month)
+    cur.execute('''SELECT sum(cost) from %s_expense_table where month = %s''' % (user, month))
     mon_val = cur.fetchone()
     return str(lim_val[0]-mon_val[0])
-"""
 
 
-@app.route('/expense/items/<string:item>/<int:cost>/<string:category>', methods=['POST'])
-def add_expense(item, cost, category):
+
+@app.route('/<string:user>/expense/items/<string:item>/<int:cost>/<string:category>', methods=['POST'])
+def add_expense(user, item, cost, category):
         cur = mysql.connection.cursor()
-        cur.execute('''SELECT MAX(id) from expense_table''')
+        cur.execute('''SELECT MAX(id) from %s_expense_table'''%user)
         maxid = cur.fetchone()
         ID = maxid[0]
         if not maxid[0]:
@@ -195,28 +224,29 @@ def add_expense(item, cost, category):
         now = datetime.datetime.now()
         month = "%d%d" %(now.year, now.month)
         month = month[2:]
-        cur.execute('''INSERT INTO expense_table(id, name, cost, month, category) value (%s ,"%s", %s, %s, "%s")''' %(ID+1, item, cost, month, category))
+        cur.execute('''INSERT INTO %s_expense_table(id, name, cost, month, category) value (%s ,"%s", %s, %s, "%s")''' %(user, ID+1, item, cost, month, category))
         mysql.connection.commit()
         return "Added %s!" %item
 
 
-@app.route('/expense/limit/<int:mon_lim>', methods=['PUT'])
-def update_limit(mon_lim):
+@app.route('/<string:user>/expense/limit/<int:mon_lim>', methods=['PUT'])
+def update_limit(user, mon_lim):
     cur = mysql.connection.cursor()
-    cur.execute('''UPDATE limit_table set mon_lim = %d where name = "DEFAULT"''' % (mon_lim))
+    cur.execute('''UPDATE %s_limit_table set mon_lim = %d where name = "DEFAULT"''' % (user, mon_lim))
     mysql.connection.commit()
     return "Updated monthly limit to %s!" %mon_lim
 
 
-@app.route('/expense/items/<string:item>/<int:month>/<int:cost>', methods=['DELETE', 'PUT'])
-def delete_item(item, month, cost):
+
+@app.route('/<string:user>/expense/items/<string:item>/<int:month>/<int:cost>', methods=['DELETE', 'PUT'])
+def delete_item(user, item, month, cost):
     cur = mysql.connection.cursor()
 
     if request.method == 'DELETE':
         if cost == 0:
-            cur.execute('''DELETE from expense_table where name = "%s" and month = "%s"''' %(item, month))
+            cur.execute('''DELETE from %s_expense_table where name = "%s" and month = "%s"''' %(user, item, month))
         else:
-            cur.execute('''DELETE from expense_table where name = "%s" and month = "%s" and cost = "%s"''' %(item, month, cost))
+            cur.execute('''DELETE from %s_expense_table where name = "%s" and month = "%s" and cost = "%s"''' %(user, item, month, cost))
         mysql.connection.commit()
         return "Deleted %s!" %item
     elif request.method == 'PUT':
@@ -224,6 +254,133 @@ def delete_item(item, month, cost):
         mysql.connection.commit()
         return "Updated %s!" %item
 
+
+
+#######################USER INFO#################################################
+
+
+@app.route('/users/<string:name>/<string:passwd>', methods=['POST', 'DELETE', 'GET'])
+def manage_user(name, passwd):
+        cur = mysql.connection.cursor()
+
+        if request.method == 'GET':
+                cur.execute('''SELECT password from user_table where name = "%s"''' % name)
+                retVal = cur.fetchone()
+                if retVal:
+                    if retVal[0]==passwd:
+                        return "LOGGED"
+                    else:
+                        return "WRONG"
+                    #return str(json.dumps({"output":output}))
+                else:
+                    return "FAILED"
+                    #return str(json.dumps({"fail":"User Doesn't Exist"}))
+
+        elif request.method == 'POST':
+
+            cur.execute('''SELECT name from user_table where name = "%s"''' % name)
+            if cur.fetchone():
+                return "EXISTS"
+
+
+            cur.execute('''SELECT MAX(id) from user_table''')
+            maxid = cur.fetchone()
+            ID = maxid[0]
+            if not maxid[0]:
+                ID = -1
+            cur.execute('''INSERT INTO user_table(id, name, password) value (%s ,"%s", "%s")''' % (ID + 1, name, passwd))
+            mysql.connection.commit()
+            return "CREATED"
+
+        elif request.method == 'DELETE':
+            cur.execute('''DELETE from user_table where name = "%s" and password = "%s"''' % (name, passwd))
+            mysql.connection.commit()
+            return "DELETED %s!" % name
+
+        return "Did nothing!"
+
+
+"""@app.route('/users/<string:name>/<string:passwd>', methods=['POST', 'DELETE', 'GET'])
+def manage_user(name, passwd):
+        cur = mysql.connection.cursor()
+
+        if request.method == 'GET':
+                cur.execute('''SELECT password from user_table where name = "%s"''' % name)
+                retVal = cur.fetchone()
+                output = retVal[0]==passwd
+                return str(json.dumps({"output":output}))
+
+        elif request.method == 'POST':
+
+            cur.execute('''SELECT MAX(id) from user_table''')
+            maxid = cur.fetchone()
+            ID = maxid[0]
+            if not maxid[0]:
+                ID = -1
+            cur.execute('''INSERT INTO user_table(id, name, password) value (%s ,"%s", "%s")''' % (ID + 1, name, passwd))
+            mysql.connection.commit()
+            return "CREATED %s!" % name
+
+        elif request.method == 'DELETE':
+            cur.execute('''DELETE from user_table where name = "%s" and password = "%s"''' % (name, passwd))
+            mysql.connection.commit()
+            return "DELETED %s!" % name
+
+        return "Did nothing!"
+"""
+
+@app.route('/RESET/<string:name>', methods=['PUT'])
+def reset_tables(name):
+        cur = mysql.connection.cursor()
+
+        if request.method == 'PUT':
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_expense_table"))
+            mysql.connection.commit()
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_shoplist_table"))
+            mysql.connection.commit()
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_limit_table"))
+            mysql.connection.commit()
+
+            cur.execute(''' create table %s(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), cost INT, month INT, category VARCHAR(20))''' % (name + "_expense_table"))
+            mysql.connection.commit()
+
+            cur.execute('''create table %s(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), priority VARCHAR(10))''' % (name + "_shoplist_table"))
+            mysql.connection.commit()
+
+            cur.execute('''create table %s(name VARCHAR(20), mon_lim INT)''' % (name + "_limit_table"))
+            mysql.connection.commit()
+
+            cur.execute('''insert into %s(name, mon_lim) value ("DEFAULT", 1000)''' % (name + "_limit_table"))
+            mysql.connection.commit()
+
+            return "REFRESHED"
+
+        return "FAILED"
+
+@app.route('/DELETE/<string:name>', methods=['DELETE'])
+def delete_user(name):
+        cur = mysql.connection.cursor()
+
+        if request.method == 'DELETE':
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_expense_table"))
+            mysql.connection.commit()
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_shoplist_table"))
+            mysql.connection.commit()
+            cur.execute('''DROP TABLE IF EXISTS `%s`''' % (name+"_limit_table"))
+            mysql.connection.commit()
+            cur.execute('''DELETE from user_table where name = "%s"''' % (name))
+            mysql.connection.commit()
+
+            return "DELETED %s!" % name
+
+        return "Did nothing!"
+
+
+
+
+
+
+########################MAIN #################################################3
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 35741, debug=True)
